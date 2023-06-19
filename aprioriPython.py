aa73fda2a1e216ef082783c8082175b99201aca3
Monte D <monte@csu.fullerton.edu>
2023-06-19 03:04:49 -0700

    idk

--- aprioriPython.py
+++ aprioriPython.py
@@ -20,6 +20,7 @@ attribute_names = [x.replace('-', '_') for x in attribute_names]
 df2 = pd.read_csv("house-votes-84.data", names=attribute_names)
 print(df2.shape)
 
+
 df2 = [
     ['republican', 'n', 'y', 'n', 'y', 'y', 'y', 'n', 'n', 'n', 'y', '?', 'y', 'y', 'y', 'n', 'y'],
     ['republican', 'n', 'y', 'n', 'y', 'y', 'y', 'n', 'n', 'n', 'n', 'n', 'y', 'y', 'y', 'n', '?'],
@@ -42,11 +43,15 @@ df2 = [
 ]
 df2 = pd.DataFrame(df2, columns=attribute_names)
 
+df2 = df2[["class_name", "handicapped_infants", "water_project_cost_sharing", "religious_groups_in_schools", "crime"]]
+
+print(df2.shape)
+
 # Replace '?' with NaN
 df2.replace('?', np.nan, inplace=True)
 
 # Convert 'y' and 'n' to 1 and 0
-df2.replace({'y': 1, 'n': 0}, inplace=True)
+#df2.replace({'y': 1, 'n': 0}, inplace=True)
 #df.replace('?', 'unknown', inplace=True)
 
 # Drop rows with missing values
@@ -55,28 +60,31 @@ print(df2.shape)
 # Reset the index
 df2.reset_index(drop=True, inplace=True)
 
-df2 = df2[["class_name", "handicapped_infants", "water_project_cost_sharing",
-  "adoption_of_the_budget_resolution", "physician_fee_freeze",
-  "el_salvador_aid"]]
-
 # Drop the class_name column
-df = df2.drop('class_name', axis=1, inplace=False)
+df_without_class = df2.drop('class_name', axis=1, inplace=False)
+
+df_encoded = pd.get_dummies(df_without_class)
+
+
+#df2 = df2[["class_name", "handicapped_infants", "water_project_cost_sharing",
+#  "adoption_of_the_budget_resolution", "physician_fee_freeze",
+#  "el_salvador_aid"]]
 
 # Convert all rows to 1 or 0 factors
-df = df.astype('category')
+#df = df.astype('category')
 #df_encoded = pd.get_dummies(df)
 
 # Apply the Apriori algorithm
-start = time.time()
-frequent_itemsets = apriori(df, min_support=0.1, use_colnames=True)
-end = time.time()
+#start = time.time()
+#frequent_itemsets = apriori(df_encoded, min_support=0.5, use_colnames=True)
+#end = time.time()
 # Sort by support
-frequent_itemsets.sort_values(by='support', ascending=False, inplace=True)
-print("Time elapsed: ", end - start)
-print(frequent_itemsets.shape)
+#frequent_itemsets.sort_values(by='support', ascending=False, inplace=True)
+#print("Time elapsed: ", end - start)
+#print(frequent_itemsets.shape)
 #print(frequent_itemsets)
 
-frequent_itemsets.to_csv("frequent_itemsets_apriori.csv", index=False)
+#frequent_itemsets.to_csv("frequent_itemsets_apriori.csv", index=False)
 #print(frequent_itemsets)
 # Generate association rules
 #rules = association_rules(frequent_itemsets, metric="confidence", min_threshold=0.6)
@@ -90,22 +98,24 @@ frequent_itemsets.to_csv("frequent_itemsets_apriori.csv", index=False)
 from mlxtend.frequent_patterns import fpgrowth
 
 start = time.time()
-frequent_itemsets = fpgrowth(df, min_support=0.1, use_colnames=True)
+frequent_itemsets = fpgrowth(df_encoded, min_support=0.3, use_colnames=True)
 end = time.time()
 print(frequent_itemsets.shape)
 print("Time elapsed: ", end - start)
 frequent_itemsets.sort_values(by='support', ascending=False, inplace=True)
 frequent_itemsets.to_csv("frequent_itemsets_fpgrowth.csv", index=False)
 
+# Add class_name to df_encoded
+df_encoded['class_name'] = df2['class_name']
 
 # Class Association Rules
-def class_assocation_rule(df, freq_itemsets, class_name, min_confidence):
+def class_assocation_rule(df_og, freq_itemsets, class_name, min_confidence):
     """
     Get the class association rules.
 
     Parameters
     ----------
-    df : pandas DataFrame
+    df_og : pandas DataFrame
         The dataset.
     freq_itemsets : pandas DataFrame
         The frequent itemsets.
@@ -120,8 +130,8 @@ def class_assocation_rule(df, freq_itemsets, class_name, min_confidence):
         The class association rules.
     """
     result = []
-    num_rows = df.shape[0]
-    classes = df[class_name].unique()
+    num_rows = df_og.shape[0]
+    classes = df_og[class_name].unique()
     for c in classes:
         for index, row in freq_itemsets.iterrows():
             leftside = set(row['itemsets'])
@@ -133,14 +143,15 @@ def class_assocation_rule(df, freq_itemsets, class_name, min_confidence):
             for item in leftside:
               query_expression = query_expression + item + " == 1 & "
             query_expression = query_expression[:-2]
-            confidence = len(df.query(query_expression)) / support_denominator
+            confidence = len(df_og.query(query_expression)) / support_denominator
             if confidence >= min_confidence:
                 result.append({"left":leftside, "right":rightside, "support":support, "confidence": confidence})
 
     return pd.DataFrame(result)
 
 start = time.time()
-car = class_assocation_rule(df2, frequent_itemsets, "class_name", min_confidence=0.5)
+
+car = class_assocation_rule(df_encoded, frequent_itemsets, "class_name", min_confidence=0.2)
 end = time.time()
 print("Time elapsed: ", end - start)
 # Sort 
